f0313bf287da98a6bcf0281c92bbe508c3df9f6b
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-12-23 11:33:21 +0000

    tidy up the main script a little bit

--- pages2docs.R
+++ pages2docs.R
@@ -1,15 +1,13 @@
+library(purrr)
+library(readr)
+
 source("R/mkdocs_pages_out.R")
 source("R/read-pages.R")
 
-pages_path <- file.path(Sys.getenv("HOME"), "001_Project", "Writing", "Memoir", "pages")
+pages_out <- file.path(Sys.getenv("PAGESPATH")) |>
+  read_pages(years = 2023) |>
+  mkdocs_pages_out()
 
-pages <- read_pages(pages_path, years = 2020:2023)
+walk(pages_out$new_dir, dir.create, recursive = TRUE, showWarnings = FALSE)
 
-pages_out <- pages |>
-    #filter(lubridate::year(ymd) %in% c(2019, 2018, 2017)) %>%
-    #filter(lubridate::month(ymd) %in% c(6, 5, 4)) %>%
-    mkdocs_pages_out()
-
-purrr::walk(pages_out$new_dir, dir.create, recursive = TRUE, showWarnings = FALSE)
-
-purrr::walk2(pages_out$new_text, pages_out$new_path, readr::write_file)
+walk2(pages_out$new_text, pages_out$new_path, write_file)
