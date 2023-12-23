50c13edc53a6ba5b73a9b5cda20da070f83cb881
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-12-23 11:33:06 +0000

    seems like these functions were not being used

--- R/g.R
+++
@@ -1,7 +0,0 @@
-g <- function(year) {
-  my_list <- list()
-  my_list[[paste0("20", year)]] <- sprintf("%02d", 1:12) |>
-    map(possibly(f, otherwise = NA), year = year)
-  my_list <- lapply(my_list, function(x) x[!is.na(x)])
-  return(my_list)
-}
